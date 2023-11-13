00407f0c746b5fcfed7f93a9e69c3b64827ff830
Hrushikesh R Patil <77801567+hrpatil07@users.noreply.github.com>
2023-11-13 15:03:08 +0000

    variable info

---
+++ Fundamental-Part1/variables.js
@@ -0,0 +1,29 @@
+// Output a message to the console
+console.log('JavaScript is amazing');
+
+// A value is the smallest unit of information in JavaScript. For example, 'JavaScript is amazing' is a value.
+// We can store a value in a variable, which acts like a box for storing and reusing the value.
+
+// Example of a variable:
+let js = 'JavaScript is amazing';
+// 'js' is a variable, and now we can use the 'js' variable throughout our code.
+console.log(js);
+
+/* Naming conventions and rules for variables:
+   1. Use camelCase for variable names, like firstNameOfUser.
+   2. Do not start variable names with a capital letter, e.g., avoid using Person.
+   3. If a variable's value will never change, use all uppercase letters, e.g., let PI = 3.14.
+   4. Choose descriptive variable names; for example, let myFirstJob is better than job1.
+
+   Rules:
+   1. Cannot start a variable name with a number, e.g., let 3years - ERROR.
+   2. Variables can only contain alphanumeric characters, underscores, and dollar ($) signs.
+      Anything else is not allowed. For example, let jonas$matida - ERROR.
+      Instead, use let jonas_matida.
+   3. Avoid using reserved keywords like function, let, and const as variable names.
+   4. The 'name' keyword does not give an error, but it's best to avoid using it.
+*/
+
+// Example of using uppercase for a constant value:
+const PI = 3.14;
+console.log(PI);
